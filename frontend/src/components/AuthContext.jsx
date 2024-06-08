c00dd0fdc47341bf2846e736e21199f995232b81
santhoshramarapu <santthoshramarapu15@gmail.com>
2024-06-09 00:15:54 +0530

    authentication changes

--- frontend/src/components/AuthContext.jsx
+++ frontend/src/components/AuthContext.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
     // Initialize isLoggedIn from localStorage or default to false if not found
-    return localStorage.getItem('isLoggedIn') === 'false';
+    return localStorage.getItem('isLoggedIn') === 'true';
   });
 
   const login = () => {
